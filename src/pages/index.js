2f57d7683d41b4264e4685434ee998f89ea935a7
YajanaRao <yajananrao@gmail.com>
2020-09-07 13:02:36 +0530

    Separated blogs and courses

--- src/pages/index.js
+++ src/pages/index.js
@@ -81,7 +81,10 @@ export const pageQuery = graphql`
         title
       }
     }
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+    allMarkdownRemark(
+      sort: { fields: [frontmatter___date], order: DESC }
+      filter: {fileAbsolutePath: {regex: "\\/blog/"}}
+      ) {
       edges {
         node {
           excerpt
