754024cc659ae3c5b56179dd6bd07b3dad2c80c5
Yajana N Rao <yajananrao@gmail.com>
2021-08-07 18:55:41 +0000

    updated packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -63,7 +63,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `G-0T2YRD1PT8`,
       },
     },
     `gatsby-plugin-feed`,
