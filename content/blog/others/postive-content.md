f1fe08862227c63f1f33da8a72ba46ae89d43d5d
YajanaRao <yajananrao@gmail.com>
2021-09-04 16:34:00 +0530

    minor updates to theme

--- content/blog/others/postive-content.md
+++ content/blog/others/postive-content.md
@@ -5,4 +5,10 @@ date: "2021-08-25"
 categories: "spirituality"
 ---
 
+## Being Simply Beautiful
+
+Stuff everywhere! Bags, clothes, cars, iPads. We love our stuff. And over time, we’ve come to believe that this 'stuff' is what defines who we are. But our possessions will never fully satisfy the inmost desires of our soul. They never have. And they are not about to start. In fact, most of the time, it distract us from the very things that bring meaning to our lives.
+
+Instead, our lives are defined by the choices we make. It is these choices that define our character, our authentic self. So choose wisely. Do not miss out by placing importance on things instead of people. Pursue beauty, hope, love and kindness. Pursue opportunity to improve this world for somebody else. Let’s be simply beautiful.
+
 - https://youtu.be/mRF1GUZHDIo
---
+++ src/components/switch.js
@@ -0,0 +1,55 @@
+import React from 'react';
+
+const Switch = () => {
+    const [theme, setTheme] = React.useState("light")
+    function toggleTheme() {
+        if (localStorage.theme === "dark") {
+            // Whenever the user explicitly chooses light mode
+            localStorage.theme = "light";
+            document.documentElement.classList.remove("dark");
+            document.documentElement.classList.add("light");
+            setTheme("light");
+        } else {
+            // Whenever the user explicitly chooses dark mode
+            localStorage.theme = "dark";
+            document.documentElement.classList.remove("light");
+            document.documentElement.classList.add("dark");
+            setTheme("dark");
+        }
+    }
+
+    React.useEffect(() => {
+        if (
+            localStorage.theme === "dark" ||
+            (!("theme" in localStorage) &&
+                window.matchMedia("(prefers-color-scheme: dark)").matches)
+        ) {
+            document.documentElement.classList.add("dark");
+            setTheme("dark");
+        } else {
+            document.documentElement.classList.remove("dark");
+        }
+    }, []);
+
+    return (
+        <div className="flex items-center justify-center w-full mb-12">
+            <label className="flex items-center cursor-pointer">
+                <div className="relative">
+                    <input
+                        type="checkbox"
+                        id="toggleB"
+                        className="sr-only"
+                        onChange={toggleTheme}
+                        checked={theme === "dark"}
+                        aria-label="Switch between Dark and Light mode"
+                    />
+                    <div className="block bg-gray-600 w-14 h-8 rounded-full"></div>
+                    <div className="dot absolute left-1 top-1 bg-white w-6 h-6 rounded-full transition"></div>
+                </div>
+            </label>
+        </div>
+    );
+}
+
+
+export default Switch;
