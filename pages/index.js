e0e517bb9a8798a99ddb25a3e1950ee183988efd
Yajana N Rao <yajananrao@gmail.com>
2023-11-08 20:52:27 +0530

    Using forms and search params

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,9 @@ import * as React from "react";
 import Link from "next/link";
 import SEO from "../components/seo";
 import { getSortedPostsData } from "../lib/posts";
-import { useRouter } from "next/router";
 import { useSearchParams } from "next/navigation";
 
 const BlogIndex = ({ allPosts }) => {
-  const router = useRouter();
   const searchParams = useSearchParams();
   const query = searchParams.get("q");
   let categories = React.useMemo(
@@ -35,48 +33,39 @@ const BlogIndex = ({ allPosts }) => {
     [query, allPosts]
   );
 
-  const handleChange = (event) => {
-    const query = event.target.value;
-    router.query.q = query;
-    router.push(router);
-  };
-
-  function setCategory(category) {
-    router.query.q = category;
-    router.push(router);
-  }
-
   return (
     <div>
       <SEO title="All posts" />
-      <div style={{ display: "flex" }}>
-        <input
-          type="text"
-          id="search"
-          name="Search Blogs"
-          placeholder="Search Blogs"
-          autoFocus
-          defaultValue={query}
-          onChange={handleChange}
-          className="shadow appearance-none w-full py-2 px-3 text-gray-700 leading-tight w-full rounded focus:ring-2 focus:ring-green-600"
-        />
-      </div>
+      <form>
+        <div style={{ display: "flex" }}>
+          <input
+            type="text"
+            id="search"
+            name="q"
+            placeholder="Search Blogs"
+            autoFocus
+            defaultValue={query}
+            className="shadow appearance-none w-full py-2 px-3 text-gray-700 leading-tight w-full rounded focus:ring-2 focus:ring-green-600"
+          />
+        </div>
+      </form>
       <div className="py-2 mb-4 mx-auto flex items-center">
         <div className="w-full text-center mx-auto">
-          {categories.map((category) => (
-            <button
-              key={category}
-              onClick={() => setCategory(category)}
-              type="button"
-              className={
-                query === category
-                  ? "m-1 border-transparent bg-green-600 px-2 py-1 text-sm shadow-sm font-medium tracking-wider text-green-100 rounded-full hover:shadow-2xl hover:bg-green-700"
-                  : "m-1 border-transparent bg-green-50 px-2 py-1 text-sm shadow-sm font-medium tracking-wider  text-green-600 rounded-full hover:shadow-2xl hover:bg-green-100"
-              }
-            >
-              {`#${category}`}
-            </button>
-          ))}
+          <form>
+            {categories.map((category) => (
+              <input
+                key={category}
+                className={
+                  query === category
+                    ? "m-1 border-transparent bg-green-600 px-2 py-1 text-sm shadow-sm font-medium tracking-wider text-green-100 rounded-full hover:shadow-2xl hover:bg-green-700"
+                    : "m-1 border-transparent bg-green-50 px-2 py-1 text-sm shadow-sm font-medium tracking-wider  text-green-600 rounded-full hover:shadow-2xl hover:bg-green-100"
+                }
+                type="submit"
+                name="q"
+                value={category}
+              />
+            ))}
+          </form>
         </div>
       </div>
       {posts.map((node) => {
