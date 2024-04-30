f6e79645f2b2a2a9bad77029403057cf0bd4a027
Yajana N Rao <yajananrao@gmail.com>
2024-04-30 22:10:49 +0530

    Update favicon link in root.tsx

--- app/root.tsx
+++ app/root.tsx
@@ -1,6 +1,7 @@
 import { Links, Meta, Outlet, Scripts } from "@remix-run/react";
 import "../styles/globals.css";
 import Layout from "@/components/layout";
+import Favicon from "../public/favicon.ico";
 
 export default function App() {
   return (
@@ -8,7 +9,7 @@ export default function App() {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="data:image/x-icon;base64,AA" />
+        <link rel="icon" href={Favicon} />
         <Meta />
         <Links />
       </head>
