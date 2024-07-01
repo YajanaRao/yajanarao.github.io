3d1df0b5692839ef645ee64972b604177c74aca2
Yajana N Rao <yajananrao@gmail.com>
2024-07-01 13:34:03 +0530

    converted bold conjecture blog from draft

--- app/root.tsx
+++ app/root.tsx
@@ -9,7 +9,7 @@ export default function App() {
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta property="og:url" content="https://yajana.in" />
         <meta property="og:type" content="website" />
         <link rel="icon" href="/favicon.ico" />
