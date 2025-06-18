fdba170ff4c7000e0276ff343bc481e4ac6ab149
YajanaRao <yajananrao@gmail.com>
2025-06-18 23:17:52 +0530

    feat: blockquote color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,7 +9,15 @@ export default {
       "Fira Sans": ["Fira Sans", "sans-serif"],
       "Montserrat": ["Montserrat", "sans-serif"],
     },
-    extend: {},
+    extend: {
+      typography: () => ({
+        DEFAULT: {
+          css: {
+            '--tw-prose-quote-borders': '#059669',
+            }
+        }
+      }),
+    },
   },
   plugins: [typography],
 } satisfies Config;
